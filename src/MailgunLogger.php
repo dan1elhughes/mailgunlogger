c92ccd9c089d98df9c49d514339b1960c39fb3d7
Dan Hughes <danhughes.dh+github@gmail.com>
2015-04-23 21:00:07 +0100

    working

--- src/MailgunLogger.php
+++ src/MailgunLogger.php
@@ -17,6 +17,19 @@ class MailgunLogger {
 		$this->from = $settings['from'];
 	}
 
+	PRIVATE function generateFooter($debug) {
+		$file = $debug['file'];
+		$line = $debug['line'];
+		$ip = $_SERVER['REMOTE_ADDR'];
+
+		$f = "\n\n---\n";
+		$f .= "File: $file\n";
+		$f .= "Line: $line\n";
+		$f .= "Remote IP: $ip\n";
+
+		return $f;
+	}
+
 	public function add($type, $body) {
 		$this->mails[$type] = $body;
 	}
@@ -28,23 +41,46 @@ class MailgunLogger {
 				'from'    => 'auto@xes.io',
 				'to'      => 'auto@xes.io',
 				'subject' => "Alert : $type",
-				'text'    => $this->mails[$type]
+				'text'    => $this->mails[$type] . $this->generateFooter(debug_backtrace()[0])
 			);
 
 			if ($real) {
 				$this->dispatch($mail);
-			} else {
-				return $mail;
 			}
+
+			return $mail;
 		}
 	}
 
 	public function dispatch($settings) {
+		$mailThread = new Email($settings, $this->domain, $this->mailgun);
+		$mailThread->start();
+	}
+}
+
+class Email extends \Thread {
+	private $from;
+	private $to;
+	private $subject;
+	private $text;
+	private $mailgun;
+
+	public function __construct($settings, $domain, $mailgun) {
+		$this->from = $settings['from'];
+		$this->to = $settings['to'];
+		$this->subject = $settings['subject'];
+		$this->text = $settings['text'];
+
+		$this->domain = $domain;
+		$this->mailgun = $mailgun;
+	}
+
+	public function run() {
 		$this->mailgun->sendMessage($this->domain, array(
-			'from' => $settings['from'],
-			'to' => $settings['to'],
-			'subject' => $settings['subject'],
-			'text' => $settings['text'],
+			'from' => $this->from,
+			'to' => $this->to,
+			'subject' => $this->subject,
+			'text' => $this->text,
 		));
 	}
 }
