b9b4bea43a5674c91c4a9b8de7e5974d9cada53b
Dan Hughes <danhughes.dh+github@gmail.com>
2015-04-23 21:18:53 +0100

    Add hostname, remove threading

--- src/MailgunLogger.php
+++ src/MailgunLogger.php
@@ -17,15 +17,17 @@ class MailgunLogger {
 		$this->from = $settings['from'];
 	}
 
-	PRIVATE function generateFooter($debug) {
+	private function generateFooter($debug) {
 		$file = $debug['file'];
 		$line = $debug['line'];
 		$ip = $_SERVER['REMOTE_ADDR'];
+		$host = gethostname();
 
 		$f = "\n\n---\n";
 		$f .= "File: $file\n";
 		$f .= "Line: $line\n";
 		$f .= "Remote IP: $ip\n";
+		$f .= "Server: $host\n";
 
 		return $f;
 	}
@@ -38,51 +40,27 @@ class MailgunLogger {
 		if (array_key_exists($type, $this->mails)) {
 
 			$mail = array(
-				'from'    => 'auto@xes.io',
-				'to'      => 'auto@xes.io',
-				'subject' => "Alert : $type",
+				'from'    => $this->from,
+				'to'      => $this->to,
+				'subject' => "Alert triggered on $this->domain: $type",
 				'text'    => $this->mails[$type] . $this->generateFooter(debug_backtrace()[0])
 			);
 
 			if ($real) {
 				$this->dispatch($mail);
+			} else {
+				return $mail;
 			}
-
-			return $mail;
 		}
 	}
 
 	public function dispatch($settings) {
-		$mailThread = new Email($settings, $this->domain, $this->mailgun);
-		$mailThread->start();
-	}
-}
-
-class Email extends \Thread {
-	private $from;
-	private $to;
-	private $subject;
-	private $text;
-	private $mailgun;
-
-	public function __construct($settings, $domain, $mailgun) {
-		$this->from = $settings['from'];
-		$this->to = $settings['to'];
-		$this->subject = $settings['subject'];
-		$this->text = $settings['text'];
-
-		$this->domain = $domain;
-		$this->mailgun = $mailgun;
-	}
-
-	public function run() {
 		$this->mailgun->sendMessage($this->domain, array(
-			'from' => $this->from,
-			'to' => $this->to,
-			'subject' => $this->subject,
-			'text' => $this->text,
+			'from' => $settings['from'],
+			'to' => $settings['to'],
+			'subject' => $settings['subject'],
+			'text' => $settings['text'],
 		));
 	}
 }
-
 ?>
